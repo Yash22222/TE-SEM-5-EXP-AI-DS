d6e8727e1c5a062fd344db96c9fff84dc0d6fd8b
Yash Ashok Shirsath <97459174+Yash22222@users.noreply.github.com>
2023-09-06 22:15:43 +0530

    AIEXP5.py

--- AI LAB/EXP 5/AIEXP5.py
+++ AI LAB/EXP 5/AIEXP5.py
@@ -1,88 +1,96 @@
-def astaralgo(start_node,stop_node):
+import heapq
+import copy
+
+class PuzzleNode:
+    def __init__(self, state, parent=None, action=None, cost=0):
+        self.state = state
+        self.parent = parent
+        self.action = action
+        self.cost = cost
+        self.heuristic = self.calculate_heuristic()
+
+    def calculate_heuristic(self):
+        # Calculate the Manhattan distance heuristic
+        heuristic = 0
+        goal_state = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
+        for i in range(3):
+            for j in range(3):
+                if self.state[i][j] != 0:
+                    x, y = divmod(self.state[i][j] - 1, 3)
+                    heuristic += abs(i - x) + abs(j - y)
+        return heuristic
+
+    def __lt__(self, other):
+        return (self.cost + self.heuristic) < (other.cost + other.heuristic)
+
+def a_star(initial_state):
+    open_list = []
+    closed_set = set()
     
-    open_set = set(start_node)
-    closed_set= set()
+    start_node = PuzzleNode(initial_state)
+    heapq.heappush(open_list, start_node)
     
-    g={}
-    parents = {}
-    
-    g[start_node] = 0
-    
-    parents[start_node]  = start_node
-    
-    while len(open_set) > 0:
-        n=None
+    while open_list:
+        current_node = heapq.heappop(open_list)
         
-        for v in open_set:
-            if n==None or g[v] + heuristic(v) < g[n] + heuristic(n):
-                n=v
-        if n==stop_node or Graph_nodes[n]==None:
-                pass
-            
-        else:
-                
-            for(m,weight) in get_neighbors(n):
-                if m not in open_set  and m not in closed_set:
-                        open_set.add(m)
-                        parents[m]=n
-                        g[m] = g[n] + weight
-                else:
-                        if g[m] > g[n] +  weight:
-                            g[m] = g[n] + weight
-                            parents[m]=n
-                            
-                            if m in closed_set:
-                                closed_set.remove(m)
-                                open_set.add(m)
-        if n==None:
-            print("path does not exist!")
-            return None
-                
-        if n==stop_node:
-            path=[]
-                    
-            while parents[n]!=n:
-                path.append(n)
-                n=parents[n]
-                        
-            path.append(start_node)
-                    
-            path.reverse()
-                    
-            print("Path found: {}".format(path))
-            return path
-                
-                
-        open_set.remove(n)
-        closed_set.add(n)
-    print("Path does not exist!")
+        if current_node.state == [[1, 2, 3], [4, 5, 6], [7, 8, 0]]:
+            return reconstruct_path(current_node)
+        
+        closed_set.add(tuple(map(tuple, current_node.state)))
+        
+        for neighbor_state, action, cost in get_neighbors(current_node.state):
+            if tuple(map(tuple, neighbor_state)) not in closed_set:
+                neighbor_node = PuzzleNode(neighbor_state, current_node, action, cost)
+                heapq.heappush(open_list, neighbor_node)
+
     return None
 
-        
-def get_neighbors(v):
-    if v in Graph_nodes:
-        return Graph_nodes[v]
+def get_neighbors(state):
+    neighbors = []
+    directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    blank_i, blank_j = find_blank(state)
     
+    for dx, dy in directions:
+        new_i, new_j = blank_i + dx, blank_j + dy
+        if 0 <= new_i < 3 and 0 <= new_j < 3:
+            new_state = copy.deepcopy(state)
+            new_state[blank_i][blank_j], new_state[new_i][new_j] = new_state[new_i][new_j], new_state[blank_i][blank_j]
+            action = (dx, dy)
+            cost = 1
+            neighbors.append((new_state, action, cost))
+    
+    return neighbors
+
+def find_blank(state):
+    for i in range(3):
+        for j in range(3):
+            if state[i][j] == 0:
+                return i, j
+
+def reconstruct_path(node):
+    path = []
+    while node.parent:
+        path.insert(0, node.action)
+        node = node.parent
+    return path
+
+def print_puzzle(state):
+    for row in state:
+        print(row)
+
+if __name__ == "__main__":
+    initial_state = [[1, 2, 3], [4, 0, 6], [7, 5, 8]]
+    
+    solution = a_star(initial_state)
+    if solution:
+        print("Solution found:")
+        current_state = initial_state
+        for i, (dx, dy) in enumerate(solution):
+            print(f"Step {i + 1}:")
+            blank_i, blank_j = find_blank(current_state)
+            new_i, new_j = blank_i + dx, blank_j + dy
+            current_state[blank_i][blank_j], current_state[new_i][new_j] = current_state[new_i][new_j], current_state[blank_i][blank_j]
+            print_puzzle(current_state)
+            print()
     else:
-        return None
-    
-def heuristic(n):
-    H_dist = {
-        'A' : 11,
-        'B' : 6,
-        'C' : 99,
-        'D' : 1,
-        'E' : 7,
-        'G' : 0,
-    }  
-    return H_dist[n]
-
-
-Graph_nodes = {
-    'A' : [('B',2),('E',3)],
-    'B' : [('C',1),('G',9)],
-    'C' : None,
-    'E' : [('D',6)],
-    'D' : [('G',1)],
-}
-astaralgo('A','G')
+        print("No solution found.")
